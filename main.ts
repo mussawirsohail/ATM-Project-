ebda8a8ba2440afe51fcb6e6d2069c42256b93fd
mussawirsohail <159765711+mussawirsohail@users.noreply.github.com>
2024-04-30 12:24:55 +0500

    Add files via upload
    🏧 ATM Project with TypeScript 🚀
    
    This repository houses my implementation of an Automated Teller Machine (ATM) project built entirely using TypeScript. 💳
    
    🔍 Project Overview:
    The ATM project simulates the functionalities of a real-world ATM, allowing users to perform various banking operations such as checking balance, withdrawing funds and depositing funds. The project is designed with a user-friendly interface and incorporates robust error handling to ensure a seamless user experience.
    
    🛠️ Key Features:
    
    User Authentication: Secure user authentication system to ensure only authorized users can access their accounts.
    Account Management: Ability to create new accounts, view account details, and update account information.
    Transaction Handling: Comprehensive transaction management system for processing withdrawals and  deposits.
    Error Handling: Robust error handling mechanisms to handle edge cases and ensure data integrity.
    Data Persistence: Utilization of local storage to persist user account data between sessions.

---
+++ main.ts
@@ -0,0 +1,62 @@
+import inquirer from "inquirer"
+const mypin:number = 8585
+let mybalance:number = 35000
+
+const answer = await inquirer.prompt(
+        [
+            {
+                name: "MyPIN",
+                type: "number",
+                message: "Enter your Pin code"
+            }
+        ]
+    )
+
+    if(mypin == answer.MyPIN){
+        console.log("Correct Pin!")
+    }else{
+        console.log("Your Pin is incorrect")
+    }
+console.log(answer)
+
+const Cash_answer = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                type:"list",
+                message:"Select the option",
+                choices: ["Cash_Withdraw",  "Deposit", "Check_Balance"] 
+            }
+        ]
+    )
+        console.log(Cash_answer)
+if(Cash_answer.operation === "Cash_Withdraw"){
+    let amountAns = await inquirer.prompt(
+        [
+            {
+                name : "amount",
+                message: "enter your amount",
+                type: "number"
+
+            }
+        ]
+    )
+    mybalance -= amountAns.amount
+    console.log("your current balance is: " + mybalance)
+}
+
+if(Cash_answer.Available_balance === "Deposit"){
+    let moneydeposit = await inquirer.prompt(
+        [
+            {
+                name:"Deposited_Money",
+                type:"number",
+                message: "Money you want to deposit"
+
+            }
+        ]
+    )
+    mybalance += moneydeposit.Deposited_Money
+    console.log("Money deposited Successfully")
+    console.log("Your available balance is " + mybalance)    
+}
